c72ff83357124743e0f69cb337a82de5a3b6303b
Hana963 <2023362@brunel.ac.uk>
2024-08-25 15:01:59 +0100

    updated products

--- components/common components/Images.js
+++ components/common components/Images.js
@@ -48,6 +48,46 @@ export const getImage = (image) => {
         return require("../../assets/images/apron.jpg");
       case "exfoliant":
         return require("../../assets/images/exfoliant.png");
+      case "soap dispenser":
+        return require("../../assets/images/soap dispenser.png");
+      case "nitrile gloves":
+        return require("../../assets/images/nitrile gloves.jpg");
+      case "nitrile gloves 200":
+        return require("../../assets/images/nitrile gloves 200.jpg");
+      case "face mask aura":
+        return require("../../assets/images/face mask aura.jpg");
+      case "surgical face mask":
+        return require("../../assets/images/surgical face mask.jpg");
+      case "face shield":
+        return require("../../assets/images/face shield.jpg");
+      case "eyelash developer":
+        return require("../../assets/images/eyelash developer.jpg");
+      case "eyelash tint":
+        return require("../../assets/images/eyelash tint blue/black.jpg");
+      case "eyelash tint grey":
+        return require("../../assets/images/eyelash tint grey.jpg");
+      case "eyelash tint black":
+        return require("../../assets/images/eyelash tint black.jpg");
+      case "eyelash tint brown":
+        return require("../../assets/images/eyelash tint brown.jpg");
+      case "waxing spatulas":
+        return require("../../assets/images/waxing spatulas.jpg");
+      case "facial waxing spatulas":
+        return require("../../assets/images/facial waxing spatulas.jpg");
+      case "mini waxing spatulas":
+        return require("../../assets/images/mini waxing spatulas.jpg");
+      case "regular waxing spatulas":
+        return require("../../assets/images/regular waxing spatulas.jpg");
+      case "paper waxing strips":
+        return require("../../assets/images/paper waxing strips.jpg");
+      case "g string":
+        return require("../../assets/images/g string.jpg");
+      case "9 pack toilet tissues":
+        return require("../../assets/images/9 pack toilet tissues.jpg");
+      case "4 pack toilet tissues":
+        return require("../../assets/images/4 pack toilet tissues.jpg");
+      case "creme wax":
+        return require("../../assets/images/creme wax.jpg");
       default:
         return null;
     }
